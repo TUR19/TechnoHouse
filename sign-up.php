e95ad98a22e5cd20c39fd8d987aafa935538db3f
XMMM <turarbek191100@gmail.com>
2023-11-01 09:44:58 +0600

    Исправление оставшихся файлов

--- sign-up.php
+++ sign-up.php
@@ -19,30 +19,30 @@
 <div class="wrap">
         <div class="container">
             <header class="header">
-            <a href="Index.php" class="logo">
+            <a href="index.php" class="logo">
                 <img src="images/LOGO.svg" alt="" />
             </a>
             <ul class="nav">
                 <li class="nav_item3">
-                <a href="Index.php" class="nav_item_link">Главная</a>
+                <a href="index.php" class="nav_item_link">Главная</a>
                 </li>
                 <li class="nav_item">
-                <a href="About-us.php" class="nav_item_link">О нас</a>
+                <a href="about-us.php" class="nav_item_link">О нас</a>
                 </li>
                 <li class="nav_item">
-                <a href="Cours.php" class="nav_item_link">Курсы</a>
+                <a href="cours.php" class="nav_item_link">Курсы</a>
                 </li>
                 <li class="nav_item">
-                <a href="Comand.php" class="nav_item_link">Команда</a>
+                <a href="comand.php" class="nav_item_link">Команда</a>
                 </li>
                 <?php if (isset($_SESSION['clients'])) { ?>
                 <li class="nav_item2"> <a href="profile.php" class="nav_item_link2"><?php echo $_SESSION['clients']['full_name']; ?></a></li>
                 <?php } else { ?>
                     <li class="nav_item2">
-                        <a href="Sign-in.php" class="nav_item_link2">Войти</a>
+                        <a href="sign-in.php" class="nav_item_link2">Войти</a>
                     </li>
                     <li class="nav_item2">
-                        <a href="Sign-up.php" class="nav_item_link2">Регистрация</a>
+                        <a href="sign-up.php" class="nav_item_link2">Регистрация</a>
                     </li>
                 <?php } ?>
             </ul>
